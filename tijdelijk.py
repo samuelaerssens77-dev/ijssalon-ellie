ca5cec3b1c42760adfeabe00f3108bb6bed074b6
Samuel <sammies1977@hotmail.com>
2025-12-03 14:02:25 +0100

    vraag 3

--- tijdelijk.py
+++ tijdelijk.py
@@ -4,7 +4,8 @@ mijn_dictionary = {
     "vanille" : "4",
     "chocolade" : "5",
 }
-mijn_dictionary ["aanbieding"] = [3 * 0.8]
+a = 3 * 0.8
+mijn_dictionary ["aanbieding"] = a
 print()
 for k, v in mijn_dictionary.items():
     print(k, v)
