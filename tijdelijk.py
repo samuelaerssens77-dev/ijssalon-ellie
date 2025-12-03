71a10504db8b44abe4ce4278dc78421aa02b8420
Samuel <sammies1977@hotmail.com>
2025-12-03 13:05:37 +0100

    vraag 1

---
+++ tijdelijk.py
@@ -0,0 +1,13 @@
+prijzen = ("mijn_dictionary")
+mijn_dictionary = {
+    "aardbei" : "3",
+    "vanille" : "4",
+    "chocolade" : "5",
+}
+for k, v in mijn_dictionary.items():
+    print(k, v)
+
+
+   
+  
+   
