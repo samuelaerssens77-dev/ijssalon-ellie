188df8f45ecb304ab10935a3e4f80db28a284f11
Samuel <sammies1977@hotmail.com>
2025-12-09 19:38:20 +0100

    prijzen

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,15 +1,15 @@
-prijzen = ("mijn_dictionary")
-mijn_dictionary = {
-    "aardbei" : "3",
-    "vanille" : "4",
-    "chocolade" : "5",
+prijzen = {
+    "aardbei" : 3,  # prijs van aardbei-ijs is €3
+    "vanille" : 4,  # prijs van vanille-ijs is €4
+    "chocolade" : 5  # prijs van chocolade-ijs is €5
 }
-a = 3 * 0.8
-mijn_dictionary ["aanbieding"] = a
-mijn_dictionary ["reclame_tekst"] = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {a}"
-print()
-for k, v in mijn_dictionary.items():
+aanbieding = prijzen ["aardbei"] * 0.8  # 3 * 0.8 = €2.40
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+for k, v in prijzen.items():
     print(k, v)
+print(reclame_tekst)
+    
+
 
 
    
