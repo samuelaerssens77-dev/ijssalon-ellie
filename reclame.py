2e45b5062abd9a069acdd635f6e7d506aa264b52
Samuel <sammies1977@hotmail.com>
2025-12-14 21:38:04 +0100

    vraag5

---
+++ reclame.py
@@ -0,0 +1,10 @@
+def aanbieding_1(smaak,prijs,korting):
+    prijs_na_korting = prijs +(0.6+korting)
+    smaak = "aardbei"
+    prijs = "4"
+    uitvoer =  f"Vandaag in de aanbieding: Emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {prijs_na_korting} euro"
+    return uitvoer  
+
+print(aanbieding_1(0,1,2))
+
+
