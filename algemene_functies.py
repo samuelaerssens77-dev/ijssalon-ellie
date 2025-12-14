7ca98e9b37f6ed6666ab6b760c26d19b58ba4f64
Samuel <sammies1977@hotmail.com>
2025-12-14 12:06:27 +0100

    vraag 1

---
+++ algemene_functies.py
@@ -0,0 +1,25 @@
+def mijn_functie_1(a,b,c,d):
+   return a,b,c,d
+a = 2*2
+b = 4*4
+c = 10*10 
+d = 12*12
+
+           
+totaal = mijn_functie_1(a,b,c,d)
+
+print(totaal)
+
+
+
+    
+    
+
+
+
+
+    
+
+
+    
+
