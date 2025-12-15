7c023951f24267af16d5f0d044c60e9c7bb5a64b
Samuel <sammies1977@hotmail.com>
2025-12-15 17:39:49 +0100

    vraag7

--- reclame.py
+++ reclame.py
@@ -1,7 +1,10 @@
 def inkomsten_totaal():
-    inkomsten = 220 + 430 + 125 + 160 + 205 + 90 + 340
+    inkomsten = "220 + 430 + 125 + 160 + 205 + 90 + 340"
     return inkomsten
-totaal = inkomsten_totaal()
+inkomsten = 1570
+btw = 1570/100*9
+uitvoer = f"Het totaal van alle inkomsten van deze week is {inkomsten} euro, waarover {btw} euro btw betaald moet worden."
+totaal =  uitvoer
 
 
 print(totaal)
