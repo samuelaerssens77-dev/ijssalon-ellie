0f53517239b64613dc0d4ea58dc25dfc8881742d
Samuel <sammies1977@hotmail.com>
2025-12-15 19:58:29 +0100

    vraag9

--- reclame.py
+++ reclame.py
@@ -1,14 +1,13 @@
-def laag_en_hoog(mijn_lijst):
+def gemiddelde(mijn_lijst):
     mijn_lijst = 220, 430, 125, 160, 205, 90, 345
-    hoogste = max(mijn_lijst)
-    laagste = min(mijn_lijst)
-    return [hoogste, laagste]
-    
-  
+    totaal = sum(mijn_lijst)
+    aantal = len(mijn_lijst)
+    gemiddeld = totaal / aantal
+    return gemiddeld
    
 
     
-print(laag_en_hoog(0))
+print(gemiddelde(0))
 
 
     
