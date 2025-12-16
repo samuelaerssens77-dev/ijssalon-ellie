6943c2eb7adb646d0bf19e8cfbf10008808d5125
Samuel <sammies1977@hotmail.com>
2025-12-16 15:05:11 +0100

    vraag10

--- reclame.py
+++ reclame.py
@@ -1,15 +1,12 @@
 def gemiddelde(mijn_lijst):
-    mijn_lijst = 220, 430, 125, 160, 205, 90, 345
+    mijn_lijst = 220, 430, 125, 160, 90, 345
     totaal = sum(mijn_lijst)
     aantal = len(mijn_lijst)
     gemiddeld = totaal / aantal
-    
-    uitvoer = f"De gemiddelde inkomsten deze week zijn {gemiddeld} euro."
+
+    uitvoer = f"De gemiddelde inkomsten deze week zijn {gemiddeld} euro"
     return uitvoer
-    
 
-
-    
 print(gemiddelde(0))
 
 
