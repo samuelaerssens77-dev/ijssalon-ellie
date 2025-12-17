76556882b767f97e6260695248fe1a05bbdf9350
Samuel <sammies1977@hotmail.com>
2025-12-17 19:10:52 +0100

    vraag10

--- reclame.py
+++ reclame.py
@@ -1,10 +1,13 @@
-def laag_en_hoog(mijn_lijst):
-    mijn_lijst = [220, 430, 125, 160, 205, 90, 345]
-    hoogste_waarde = max(mijn_lijst)
-    laagste_waarde = min(mijn_lijst)
-    return [hoogste_waarde, laagste_waarde]
-inkomsten_lijst = laag_en_hoog
-print(laag_en_hoog(inkomsten_lijst))
+def gemiddelde(mijn_lijst):
+    totaal = sum(mijn_lijst)
+    aantal = len(mijn_lijst)
+    gemiddeld = totaal / aantal
+
+    uitvoer = f"De gemiddelde inkomen deze week zijn {gemiddeld} euro"
+    return uitvoer
+
+inkomsten_lijst = [220,430,125,160,90,345]
+print(gemiddelde(inkomsten_lijst))
     
     
   
