e9e6185b3110dbfc16edae21d1f9427bdd64dfa5
Samuel <sammies1977@hotmail.com>
2026-01-05 16:08:46 +0100

    laatste_vraag

--- reclame.py
+++ reclame.py
@@ -1,18 +1,15 @@
-def meervoudig(invoer_lijst):
-    invoer_lijst = 10,5,3,2,1,9,7
-    laagste = min(invoer_lijst)
-    hoogste = max(invoer_lijst)
-    totaal = (laagste, hoogste)
-    return totaal
-inkomsten_lijst = meervoudig
-print(meervoudig(inkomsten_lijst))
-    
-    
-  
-      
-   
-  
- 
-    
-    
+from algemene_functies import mijn_functie_2
 
+def combinatie(invoer_lijst_2):
+    def laag_en_hoog(invoer_lijst_2):
+        if not invoer_lijst_2:
+            return []
+        laagste = min(invoer_lijst_2)
+        hoogste = max(invoer_lijst_2)
+        return [laagste, hoogste]
+    
+    korte_lijst = laag_en_hoog(invoer_lijst_2)
+    resultaat = mijn_functie_2(korte_lijst)
+    return resultaat
+
+print(combinatie(ik_kom_er_niet_uit))
