49b1cb5a1ccce07e062a26d593833871d987af51
Samuel <sammies1977@hotmail.com>
2026-01-06 12:37:52 +0100

    vraag2

---
+++ algemene_functies1.py
@@ -0,0 +1,8 @@
+def mijn_functies_1(a,b,c,d):
+    return a,b,c,d
+a = 2*2
+b = 4*4
+c = 10*10
+d = 12*12
+print(mijn_functies_1(a,b,c,d))
+# deze had ik al gemaakt maar is niet opgeslagen (had er een foto van gemaakt)
