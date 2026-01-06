9ac2050a248bd66b0968b2502a762bcc236a730f
Samuel <sammies1977@hotmail.com>
2026-01-06 12:40:17 +0100

    vraag3

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,18 +1,18 @@
-def mijn_functie_2(a,b):
+def mijn_functie_1(a,b):
    uitvoer_lijst = []
    uitvoer_lijst.append(a+b)
    uitvoer_lijst.append(a-b)
    uitvoer_lijst.append(a*b)
    uitvoer_lijst.append(a/b)
 
-   
+   # deze had ik ook al gemaakt maar waarom is deze niet goed?
    
    return uitvoer_lijst
 
-print(mijn_functie_2(12,3))
-print(mijn_functie_2(12,2))
-print(mijn_functie_2(10,5))
-print(mijn_functie_2(100,20))
+print(mijn_functie_1(12,3))
+print(mijn_functie_1(12,2))
+print(mijn_functie_1(10,5))
+print(mijn_functie_1(100,20))
 
 
 
