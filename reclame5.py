ec18da5debd7ea6fa1d2a6e58e4fc6c6817893b6
Samuel <sammies1977@hotmail.com>
2026-01-06 13:45:09 +0100

    vraag6

--- reclame5.py
+++ reclame5.py
@@ -2,6 +2,7 @@ def aanbieding_1(smaak, prijs, korting):
     prijs_na_korting = prijs + (0.6 + korting)
     smaak = "aardbei"
     prijs = "4"
-    uitvoer = f"Vandag in de aanbieding: Emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {prijs_na_korting} euro"
+    uitvoer = f"Vandaag in de aanbieding: Emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {prijs_na_korting} euro"
     return uitvoer
 print(aanbieding_1(0,1,2))
+# deze had ik al gemaakt heb er ook een foto van alleen bij lijn 7 kom ik er niet uit
---
+++ reclame6.py
@@ -0,0 +1,9 @@
+def inkomsten_totaal(inkomsten):
+    totaal = sum(inkomsten)
+    return totaal
+dagelijke_inkomsten = [220, 430, 125, 160, 205, 90, 345]
+totale_opbrengst = inkomsten_totaal(dagelijke_inkomsten)
+print(totale_opbrengst)
+    
+        
+  
